2baa982affab37c2558746a353a0ca134a167123
loki1220 <govindlokesh1208@gmail.com>
2022-03-05 16:23:04 +0530

    initial commit

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,26 +1,27 @@
-// import 'package:animated_splash_screen/animated_splash_screen.dart';
-// import 'package:flutter/material.dart';
-//
-// import 'home_page.dart';
-//
-// class Splash extends StatefulWidget {
-//   const Splash({Key? key}) : super(key: key);
-//
-//   @override
-//   _SplashState createState() => _SplashState();
-// }
-//
-// class _SplashState extends State<Splash> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return AnimatedSplashScreen(
-//       duration: 3000,
-//       splash: Image.asset(
-//         "assets/splash.png",
-//       ),
-//       splashIconSize: double.infinity,
-//       splashTransition: SplashTransition.fadeTransition,
-//       nextScreen: LoginPage(),
-//     );
-//   }
-// }
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+import 'login_page.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      duration: 3000,
+      splash: Image.asset(
+        "assets/splash.png",
+      ),
+      splashIconSize: double.infinity,
+      splashTransition: SplashTransition.fadeTransition,
+      nextScreen: LoginPage(),
+    );
+  }
+}
